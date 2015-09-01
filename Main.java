0560e907f0d0e75083cf4e473b918e55ce4e3d2c
danielsmithu <smi14089@uvg.edu.gt>
2015-08-31 22:12:18 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+import java.util.*;
+import java.util.stream.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner lectura = new Scanner(System.in);
+    System.out.println("Bienvenido, por favor elija la implementacion que desea utilizar");
+    System.out.println("Escriba el numero de la implementacion")
+    System.out.println("1. HashSet")
+    System.out.println("2. TreeSet")
+    System.out.println("3. LinkedHashSet")
+    String implementacion = lectura.nextLine();
+    if (implementacion ==  "1"){
+      Collection<Type> hash = new HashSet<Type>(c);
+    }
+  }
+}
