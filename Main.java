6081da7ad523312a868e22484945d755d3317398
ricardogalindop <r.galindo95@gmail.com>
2015-08-31 22:38:38 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.*;
 import java.util.stream.*;
+import java.util.Iterator;
+import java.util.TreeSet;
 public class Main{
   public static void main(String[] args){
     Scanner lectura = new Scanner(System.in);
@@ -12,6 +14,10 @@ public class Main{
     String implementacion = lectura.nextLine();
     if (implementacion ==  "1"){
       Collection<Type> hash = new HashSet<Type>(c);
+    }
+    else if (implementacion == "2"){
+      System.out.println("Ud ha elegido la implementacon TreeSet")
+      
     }
   }
 }
