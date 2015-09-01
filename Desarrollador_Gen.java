0b92216be067af627248028ceaa67e38d8eb73ef
ricardogalindop <r.galindo95@gmail.com>
2015-08-31 22:31:51 -0600

    Update Desarrollador_Gen.java

--- Desarrollador_Gen.java
+++ Desarrollador_Gen.java
@@ -4,13 +4,13 @@ public class Desarrollador_Gen{
          return null;
       }		
       if(type.equalsIgnoreCase("WEB")){
-         return new Desarrollador_Web();
+         return new Desarrollador_Web(nombre);
          
       } else if(type.equalsIgnoreCase("JAVA")){
-         return new Desarrollador_Java();
+         return new Desarrollador_Java(nombre);
          
       } else if(type.equalsIgnoreCase("CEL")){
-         return new Desarrollador_Cel();
+         return new Desarrollador_Cel(nombre);
       }
       
       return null;
