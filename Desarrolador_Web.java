5ea2018864c7e453a079a3a6176749f70ee59c5b
ricardogalindop <r.galindo95@gmail.com>
2015-08-31 22:09:26 -0600

    Update Desarrolador_Web.java

--- Desarrolador_Web.java
+++ Desarrolador_Web.java
@@ -1,3 +1,5 @@
 public class Desarrolador_Web implements Desarrollador{
-  public void nuevo_desarrollador();
+  public void nuevo_desarrollador(String nombre);
+    System.out.println(nombre);
+    
 }
