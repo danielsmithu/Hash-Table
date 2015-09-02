2731cd6193351d83ac5c0a1c5a09203b01c93f09
danielsmithu <smi14089@uvg.edu.gt>
2015-09-01 18:14:51 -0600

    Update Desarrollador_Java.java

--- Desarrollador_Java.java
+++ Desarrollador_Java.java
@@ -1,4 +1,4 @@
-public class Desarrollador_Java implements Disenador{
+public class Desarrollador_Java implements Desarrollador{
   public void nuevo_desarrollador(String nombre){
      this.nombre = nombre
   }
