0e176e8c21598fdbdfbee7aecc44d848b64474ca
danielsmithu <smi14089@uvg.edu.gt>
2015-09-01 23:04:21 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -239,11 +239,11 @@ public class Main{
     }
     
     System.out.println("\nLos desarrolladores Java es un subconjunto de desarrolladores Web?");
-    if(nombre_Implementacion.equals("HashSet"){
+    if(nombre_Implementacion.equals("HashSet")){
     	int comparacion = 0;
-    	for(int a=0,i<nombres_Programadores_Web_Hash.size(),i++){
-    		for(int i=0,i<nombres_Programadores_Java_Hash.size(),i++){
-    			if(nombres_Programadores_Web_Hash.contains(nombres_Programadores_Java_Hash[i]){
+    	for(int a=0; a<nombres_Programadores_Web_Hash.size(); a++){
+    		for(int i=0;i<nombres_Programadores_Java_Hash.size();i++){
+    			if(nombres_Programadores_Web_Hash.contains(nombres_Programadores_Java_Hash[i])){
     				comparacion = comparacion+1;	
     			}
     	}
@@ -255,7 +255,7 @@ public class Main{
     		System.out.println("No todos los desarrolladores java son desarrolladores web, no es un subconjunto");
     	}
     }
-    if(nombre_Implementacion.equals("TreeSet"){
+    if(nombre_Implementacion.equals("TreeSet")){
     	
     }
     
