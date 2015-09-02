8a93dc0796b4babb34c03356675d700faef0ded5
danielsmithu <smi14089@uvg.edu.gt>
2015-09-01 20:32:14 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ import java.util.stream.*;
 import java.util.Iterator;
 import java.util.TreeSet;
 import java.util.HashSet;
-public class main_set{
+public class Main{
   public static void main(String[] args){
     Scanner lectura = new Scanner(System.in);
-    //Desarrollador_Gen desarrollador_Gen = new Desarrollador_Gen();
+    Desarrollador_Gen desarrollador_gen = new Desarrollador_Gen();
     
     boolean programadores = true;
     boolean isProgramador_Java;
     boolean isProgramador_Web;
     boolean isProgramador_Cel;
     String programador_Lectura;
-    HashSet<String> nombres_Programadores_Java_Hash = new HashSet<>();
+	  HashSet<String> nombres_Programadores_Java_Hash = new HashSet<>();
     HashSet<String> nombres_Programadores_Web_Hash = new HashSet<>();
     HashSet<String> nombres_Programadores_Cel_Hash = new HashSet<>();
     HashSet<String> union_Java_Web_Cel_Hash = new HashSet<>();
@@ -23,27 +23,27 @@ public class main_set{
     HashSet<String> union_Web_o_Cel_Hash = new HashSet<>();
     HashSet<String> mayor_Hash = new HashSet<>();
     
-    TreeSet<Integer> nombres_Programadores_Java_Tree = new TreeSet<Integer>();
-    TreeSet<Integer> nombres_Programadores_Web_Tree = new TreeSet<Integer>();
-    TreeSet<Integer> nombres_Programadores_Cel_Tree = new TreeSet<Integer>();
-    TreeSet<Integer> union_Java_Web_Cel_Tree = new TreeSet<Integer>();
-    TreeSet<Integer> union_Java_Tree = new TreeSet<Integer>();
-    TreeSet<Integer> union_Web_Cel_Tree = new TreeSet<Integer>();
-    TreeSet<Integer> union_Web_o_Cel_Tree = new TreeSet<Integer>();
-    TreeSet<Integer> mayor_Tree = new TreeSet<Integer>();
+    TreeSet<String> nombres_Programadores_Java_Tree = new TreeSet<String>();
+    TreeSet<String> nombres_Programadores_Web_Tree = new TreeSet<String>();
+    TreeSet<String> nombres_Programadores_Cel_Tree = new TreeSet<String>();
+    TreeSet<String> union_Java_Web_Cel_Tree = new TreeSet<String>();
+    TreeSet<String> union_Java_Tree = new TreeSet<String>();
+    TreeSet<String> union_Web_Cel_Tree = new TreeSet<String>();
+    TreeSet<String> union_Web_o_Cel_Tree = new TreeSet<String>();
+    TreeSet<String> mayor_Tree = new TreeSet<String>();
     
-    LinkedHashSet nombres_Programadores_Java_Lk = new LinkedHashSet();
-    LinkedHashSet nombres_Programadores_Web_Lk = new LinkedHashSet();
-    LinkedHashSet nombres_Programadores_Cel_Lk = new LinkedHashSet();
-    LinkedHashSet union_Java_Web_Cel_Lk = new LinkedHashSet();
-    LinkedHashSet union_Java_Lk = new LinkedHashSet();
-    LinkedHashSet union_Web_Cel_Lk = new LinkedHashSet();
-    LinkedHashSet union_Web_o_Cel_Lk = new LinkedHashSet();
-    LinkedHashSet mayor_Lk = new LinkedHashSet();
+    LinkedHashSet<String> nombres_Programadores_Java_Lk = new LinkedHashSet<String>();
+    LinkedHashSet<String> nombres_Programadores_Web_Lk = new LinkedHashSet<String>();
+    LinkedHashSet<String> nombres_Programadores_Cel_Lk = new LinkedHashSet<String>();
+    LinkedHashSet<String> union_Java_Web_Cel_Lk = new LinkedHashSet<String>();
+    LinkedHashSet<String> union_Java_Lk = new LinkedHashSet<String>();
+    LinkedHashSet<String> union_Web_Cel_Lk = new LinkedHashSet<String>();
+    LinkedHashSet<String> union_Web_o_Cel_Lk = new LinkedHashSet<String>();
+    LinkedHashSet<String> mayor_Lk = new LinkedHashSet<String>();
 	
-  	Desarrollador desarrollador_Java;
-	  Desarrollador desarrollador_Web;
-	  Desarrollador desarrollador_Cel;
+  	Desarrollador desarrollador_Java = desarrollador_gen.getType("JAVA");
+	Desarrollador desarrollador_Web = desarrollador_gen.getType("WEB");
+	Desarrollador desarrollador_Cel = desarrollador_gen.getType("CEL");
 	  
     
     System.out.println("Bienvenido, por favor elija la implementacion que desea utilizar");
@@ -102,88 +102,85 @@ public class main_set{
         }
         
         if(isProgramador_Java){
-          desarrollador_Java = desarrollador_Gen("JAVA", programador);
           if(nombre_Implementacion.equals("HashSet")){
-            nombres_Programadores_Java_Hash.add(desarrollador_Java.nuevo_desarrollador());
+            nombres_Programadores_Java_Hash.add(desarrollador_Java.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("TreeSet")){
-            nombres_Programadores_Java_Tree.add(desarrollador_Java.nuevo_desarrollador());
+            nombres_Programadores_Java_Tree.add(desarrollador_Java.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("LinkedHashSet")){
-            nombres_Programadores_Java_Lk.add(desarrollador_Java.nuevo_desarrollador());
+            nombres_Programadores_Java_Lk.add(desarrollador_Java.nuevo_desarrollador(programador));
           }
         }
         if(isProgramador_Web){
-          desarrollador_Web = desarrollador_Gen("WEB", programador);
           if(nombre_Implementacion.equals("HashSet")){
-            nombres_Programadores_Web_Hash.add(desarrollador_Web.nuevo_desarrollador());
+            nombres_Programadores_Web_Hash.add(desarrollador_Web.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("TreeSet")){
-            nombres_Programadores_Web_Tree.add(desarrollador_Web.nuevo_desarrollador());
+            nombres_Programadores_Web_Tree.add(desarrollador_Web.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("LinkedHashSet")){
-            nombres_Programadores_Web_Lk.add(desarrollador_Web.nuevo_desarrollador());
+            nombres_Programadores_Web_Lk.add(desarrollador_Web.nuevo_desarrollador(programador));
           }
         }
         if(isProgramador_Cel){
-          desarrollador_Cel = desarrollador_Gen("CEL", programador);
           if(nombre_Implementacion.equals("HashSet")){
-            nombres_Programadores_Cel_Hash.add(desarrollador_Cel.nuevo_desarrollador());
+            nombres_Programadores_Cel_Hash.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("TreeSet")){
-            nombres_Programadores_Cel_Tree.add(desarrollador_Cel.nuevo_desarrollador());
+            nombres_Programadores_Cel_Tree.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("LinkedHashSet")){
-            nombres_Programadores_Cel_Lk.add(desarrollador_Cel.nuevo_desarrollador());
+            nombres_Programadores_Cel_Lk.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
         }
         
         if(isProgramador_Java && isProgramador_Web && isProgramador_Cel){
           if(nombre_Implementacion.equals("HashSet")){
-            union_Java_Web_Cel_Hash.add(desarrollador_Java.nuevo_desarrollador());
-            union_Java_Web_Cel_Hash.add(desarrollador_Web.nuevo_desarrollador());
-            union_Java_Web_Cel_Hash.add(desarrollador_Cel.nuevo_desarrollador());
+            union_Java_Web_Cel_Hash.add(desarrollador_Java.nuevo_desarrollador(programador));
+            union_Java_Web_Cel_Hash.add(desarrollador_Web.nuevo_desarrollador(programador));
+            union_Java_Web_Cel_Hash.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("TreeSet")){
-            union_Java_Web_Cel_Tree.add(desarrollador_Java.nuevo_desarrollador());
-            union_Java_Web_Cel_Tree.add(desarrollador_Web.nuevo_desarrollador());
-            union_Java_Web_Cel_Tree.add(desarrollador_Cel.nuevo_desarrollador());
+            union_Java_Web_Cel_Tree.add(desarrollador_Java.nuevo_desarrollador(programador));
+            union_Java_Web_Cel_Tree.add(desarrollador_Web.nuevo_desarrollador(programador));
+            union_Java_Web_Cel_Tree.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("LinkedHashSet")){
-            union_Java_Web_Cel_Lk.add(desarrollador_Java.nuevo_desarrollador());
-            union_Java_Web_Cel_Lk.add(desarrollador_Web.nuevo_desarrollador());
-            union_Java_Web_Cel_Lk.add(desarrollador_Cel.nuevo_desarrollador());
+            union_Java_Web_Cel_Lk.add(desarrollador_Java.nuevo_desarrollador(programador));
+            union_Java_Web_Cel_Lk.add(desarrollador_Web.nuevo_desarrollador(programador));
+            union_Java_Web_Cel_Lk.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
         }
         
         if(isProgramador_Java && (isProgramador_Web==false)){
           if(nombre_Implementacion.equals("HashSet")){
-            union_Java_Hash.add(desarrollador_Java.nuevo_desarrollador());
+            union_Java_Hash.add(desarrollador_Java.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("TreeSet")){
-            union_Java_Tree.add(desarrollador_Java.nuevo_desarrollador());
+            union_Java_Tree.add(desarrollador_Java.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("LinkedHashSet")){
-            union_Java_Lk.add(desarrollador_Java.nuevo_desarrollador());
+            union_Java_Lk.add(desarrollador_Java.nuevo_desarrollador(programador));
           }
         }
         
-        if(isPogramador_Web && isProgramador_Cel){
+        if(isProgramador_Web && isProgramador_Cel && (isProgramador_Java == false)){
           if(nombre_Implementacion.equals("HashSet")){
-            union_Web_Cel_Hash.add(desarrollador_Web.nuevo_desarrollador());
-            union_Web_Cel_Hash.add(desarrollador_Cel.nuevo_desarrollador());
+            union_Web_Cel_Hash.add(desarrollador_Web.nuevo_desarrollador(programador));
+            union_Web_Cel_Hash.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("TreeSet")){
-            union_Web_Cel_Tree.add(desarrollador_Web.nuevo_desarrollador());
-            union_Web_Cel_Tree.add(desarrollador_Cel.nuevo_desarrollador());
+            union_Web_Cel_Tree.add(desarrollador_Web.nuevo_desarrollador(programador));
+            union_Web_Cel_Tree.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
           else if(nombre_Implementacion.equals("LinkedHashSet")){
-            union_Web_Cel_Lk.add(desarrollador_Web.nuevo_desarrollador());
-            union_Web_Cel_Lk.add(desarrollador_Cel.nuevo_desarrollador());
+            union_Web_Cel_Lk.add(desarrollador_Web.nuevo_desarrollador(programador));
+            union_Web_Cel_Lk.add(desarrollador_Cel.nuevo_desarrollador(programador));
           }
         }
         
-        if((isProgramador_Web || isProgramador_Cel) && (isProgramador_Java==false)){
+        if(((isProgramador_Web && (isProgramador_Cel == false)) || (isProgramador_Cel && (isProgramador_Web == false))) && (isProgramador_Java==false)){
           if(nombre_Implementacion.equals("HashSet")){
             union_Web_o_Cel_Hash.add(programador);
           }
@@ -197,7 +194,7 @@ public class main_set{
         
       }
     }
-    System.out.println("Desarrolladores Java, Web y de Celulares (" + nombre_Implementacion + "): ");
+    System.out.println("\nDesarrolladores Java, Web y de Celulares (" + nombre_Implementacion + "): ");
     if(nombre_Implementacion.equals("HashSet")){
       System.out.println(union_Java_Web_Cel_Hash);
     }
@@ -208,7 +205,7 @@ public class main_set{
       System.out.println(union_Java_Web_Cel_Lk);
     }
     
-    System.out.println("Desarrolladores de Java sin experiencia Web (" + nombre_Implementacion + "): ");
+    System.out.println("\nDesarrolladores de Java sin experiencia Web (" + nombre_Implementacion + "): ");
     if(nombre_Implementacion.equals("HashSet")){
       System.out.println(union_Java_Hash);
     }
@@ -219,7 +216,7 @@ public class main_set{
       System.out.println(union_Java_Lk);
     }
     
-    System.out.println("Desarrolladores Web y de Celulares sin experiencia Java (" + nombre_Implementacion + "): ");
+    System.out.println("\nDesarrolladores Web y de Celulares sin experiencia Java (" + nombre_Implementacion + "): ");
     if(nombre_Implementacion.equals("HashSet")){
       System.out.println(union_Web_Cel_Hash);
     }
@@ -230,7 +227,7 @@ public class main_set{
       System.out.println(union_Web_Cel_Lk);
     }
     
-    System.out.println("Desarrolladores Web o de Celulares (" + nombre_Implementacion + "): ");
+    System.out.println("\nDesarrolladores Web o de Celulares (" + nombre_Implementacion + "): ");
     if(nombre_Implementacion.equals("HashSet")){
       System.out.println(union_Web_o_Cel_Hash);
     }
@@ -241,9 +238,9 @@ public class main_set{
       System.out.println(union_Web_o_Cel_Lk);
     }
     
-    System.out.println("Los desarrolladores Java es un subconjunto de desarrolladores Web?");
+    System.out.println("\nLos desarrolladores Java es un subconjunto de desarrolladores Web?");
     
-    String conjunto_Mayor;
+    String conjunto_Mayor = "";
     if(nombre_Implementacion.equals("HashSet")){
       if(nombres_Programadores_Java_Hash.size() > nombres_Programadores_Web_Hash.size()){
         if(nombres_Programadores_Java_Hash.size() > nombres_Programadores_Cel_Hash.size()){
@@ -310,7 +307,7 @@ public class main_set{
         }
       }
     }
-    System.out.println("El conjunto mas grande es el de desarrolladores " + conjunto_Mayor);
+    System.out.println("\nEl conjunto mas grande es el de desarrolladores " + conjunto_Mayor);
     if(nombre_Implementacion.equals("HashSet")){
       System.out.println(mayor_Hash);
     }
@@ -321,7 +318,7 @@ public class main_set{
       System.out.println(mayor_Lk);
     }
     
-    System.out.println("La lista ordenada:");
+    System.out.println("\nLa lista ordenada:");
     
   }
 }
